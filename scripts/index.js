dca4da58d06867d9a80cc1f7962d8aebb3d15185
Vansh Nagar <vansh.nagar.dev@gmail.com>
2025-03-18 17:57:04 +0530

    handle close and open side bar function

--- scripts/index.js
+++ scripts/index.js
@@ -228,3 +228,17 @@ overlayContent.innerHTML = ` <div
       </div>
     </div>
 `;
+
+handleCloseButtonclick = () => {
+  const button = document.querySelector(".navSideBar");
+  button.style.display = "none";
+  const sidebarButton = document.querySelector(".ri-menu-line");
+  sidebarButton.style.display = "initial";
+};
+
+handlesidebarButton = () => {
+  const button = document.querySelector(".navSideBar");
+  button.style.display = "initial";
+  const sidebarButton = document.querySelector(".ri-menu-line");
+  sidebarButton.style.display = "none";
+};
